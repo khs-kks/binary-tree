513c19ad276ccf254ae580c1095ba17e71d86e2c
khs-kks <95934427+khs-kks@users.noreply.github.com>
2023-02-07 21:19:57 +0100

    Add insert() and delete() methods

--- binary-tree.js
+++ binary-tree.js
@@ -1,4 +1,3 @@
-
 class Node {
   constructor(data) {
     this.data = data;
@@ -7,26 +6,98 @@ class Node {
   }
 }
 
+// class Tree {
+//   constructor(values) {
+//     this.root = null;
+//     this.values = values;
+//   }
+
+//   buildTree(arr) {
+//     const sortedAndUnique = [...new Set(arr)].sort((a, b) => a - b);
+
+//     const build = (start, end) => {
+//       if (start > end) return null;
+//       const mid = Math.floor((start + end) / 2);
+//       const node = new Node(sortedAndUnique[mid]);
+//       node.left = build(start, mid - 1);
+//       node.right = build(mid + 1, end);
+//       return node;
+//     };
+
+//     this.root = build(0, sortedAndUnique.length - 1);
+//     return this.root;
+//   }
+// }
+
 class Tree {
   constructor(values) {
     this.root = null;
     this.values = values;
   }
 
-  buildTree(arr) {
-    const sortedAndUnique = [...new Set(arr)].sort((a, b) => a - b);
-
-    const build = (start, end) => {
-      if (start > end) return null;
-      const mid = Math.floor((start + end) / 2);
-      const node = new Node(sortedAndUnique[mid]);
-      node.left = build(start, mid - 1);
-      node.right = build(mid + 1, end);
-      return node;
-    };
-
-    this.root = build(0, sortedAndUnique.length - 1);
+  buildTree() {
+    this.values = [...new Set(this.values)].sort((a, b) => a - b);
+    this.root = this._buildTree(0, this.values.length - 1);
     return this.root;
+  }
+
+  _buildTree(start, end) {
+    if (start > end) return null;
+
+    let mid = Math.floor((start + end) / 2);
+    let node = new Node(this.values[mid]);
+
+    node.left = this._buildTree(start, mid - 1);
+    node.right = this._buildTree(mid + 1, end);
+
+    return node;
+  }
+
+  insert(value) {
+    this._insert(this.root, value);
+  }
+
+  _insert(node, value) {
+    if (!node) return new Node(value);
+
+    if (value < node.data) {
+      node.left = this._insert(node.left, value);
+    } else {
+      node.right = this._insert(node.right, value);
+    }
+
+    return node;
+  }
+
+  delete(value) {
+    this.root = this._delete(this.root, value);
+  }
+
+  _delete(node, value) {
+    if (!node) return null;
+
+    if (value < node.data) {
+      node.left = this._delete(node.left, value);
+    } else if (value > node.data) {
+      node.right = this._delete(node.right, value);
+    } else {
+      if (!node.left) {
+        return node.right;
+      } else if (!node.right) {
+        return node.left;
+      } else {
+        let minNode = this._findMinNode(node.right);
+        node.data = minNode.data;
+        node.right = this._delete(node.right, minNode.data);
+      }
+    }
+
+    return node;
+  }
+
+  _findMinNode(node) {
+    while (node.left) node = node.left;
+    return node;
   }
 }
 
@@ -43,4 +114,8 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 const values = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 23, 324];
 const tree = new Tree(values);
 tree.buildTree(values);
+prettyPrint(tree.root);
+
+tree.insert(24);
+tree.delete(67);
 prettyPrint(tree.root);
